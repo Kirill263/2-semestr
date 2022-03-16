df40b3c42db95bd3d9d3afd07a5752e0b4cb2627
Кирилл <myakiishevkirill@gmail.com>
2022-03-17 08:00:17 +1000

    input in C++

---
+++ cin.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int Var1;
+	int Var2;
+	
+	cout<<"¬ведите первое число: ";
+	cin>>Var1; //¬вод в консоль
+	
+	cout<<"¬ведите второе число: ";
+	cin>>Var2; 
+	
+	cout<<"ѕервое число: "<<Var1<<'\n'<<"¬торое число: "<<Var2<<endl;
+	return 0;
+}
