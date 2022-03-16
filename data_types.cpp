12b753fdb98a52cffabe92ed0a486f62bb92f663
Кирилл <myakiishevkirill@gmail.com>
2022-03-17 07:56:02 +1000

    add data types in C++

---
+++ data_types.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int a = 5;
+	cout<<a<<endl;
+	a = 10;
+	cout<<a<<endl;
+	
+	string name = "Kirill";
+	cout<<name<<endl;
+	
+	double b = 1.6554;
+	cout<<b<<endl;
+	
+	char c = '%';
+	cout<<c<<endl;
+	
+	bool d = false;
+	cout<<d<<endl;
+	bool e = true;
+	cout<<e<<endl;
+}
