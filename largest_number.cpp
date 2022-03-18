23a0e39a8120aee3fe901e539b795a76bec1da0d
Кирилл <myakiishevkirill@gmail.com>
2022-03-18 15:37:37 +1000

    add largest_number

---
+++ largest_number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int a, b;
+    cout<<"Enter a: ";
+    cin>>a;
+    cout<<"Enter b: ";
+    cin>>b;
+    if (a>b)
+	{
+    	cout<<"a:"<<a<< endl;
+    }
+    else if (b>a)
+    {
+        cout<<"b:" <<b<< endl;
+    }
+    return 0;
+}  
+
