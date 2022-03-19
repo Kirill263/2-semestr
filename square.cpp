973f2c0810726b5a746477c1b7f7f92ee291ac1c
Кирилл <myakiishevkirill@gmail.com>
2022-03-19 20:38:50 +1000

    add square

---
+++ square.cpp
@@ -0,0 +1,17 @@
+//программа выводит таблицу квадратов нечетных чисел от 1 до 19
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x = 1;
+	int y;
+	int i;
+
+	for(i=1; i<=10;i++)
+	{
+		y=x*x;
+		cout<<x<<'\t'<<y<<endl; 
+		x+=2;
+	}
+	return 0;
+}
