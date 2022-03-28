e44e243e1c5b2f3c921e6ff566791c532eeb82b2
Кирилл <myakiishevkirill@gmail.com>
2022-03-28 10:24:22 +1000

    redesigned matrix to display the sum

---
+++ sum_matrix.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+  srand(time(NULL));
+  //Инициализируем переменные
+  const int row = 4;
+  const int col = 4;
+  int sum =0;
+  
+  int matrix[row][col];
+  
+  //Создание матрицы
+  for (int i=0; i<row; i++)
+  {
+    for (int j=0; j<col; j++)
+    {
+      matrix[i][j]=rand()%1+1;
+     
+    }
+  }
+  
+    //Вывод матрицы в консоль   
+    for (int i=0; i<row; i++)
+      {
+        for (int j=0; j<col; j++)
+        {
+          cout<<matrix[i][j]<<'\t';
+        }
+        cout<<endl;
+      }
+      cout<<endl;
+    
+     
+    //Суммируем по строкам
+    for (int i=0; i<row; i++)
+      {
+        for (int j=0; j<col; j++)
+        {
+         sum = sum + matrix[i][j];
+        
+        }
+     
+      }
+    
+    //Суммируем по столбцам
+    for (int i=0; i<row; i++)
+      {
+        for (int j=0; j<col; j++)
+        {
+         sum = sum + matrix[j][i];
+        
+        }
+     
+      }
+      
+
+  //Выводим сумму в консоль
+  cout<<sum;
+
+  return sum;
+}
