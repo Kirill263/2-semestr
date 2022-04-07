f89c9bfd023cd363ee224bc12e4bb919614373d8
Кирилл <myakiishevkirill@gmail.com>
2022-04-07 18:25:13 +1000

    added new variant sum_matrix and change in gitignore

---
+++ sum_matrix_2var.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+  srand(time(NULL));
+  //Инициализируем переменные
+  const int row = 3;
+  const int col = row;
+
+  int sum = 0;
+  
+  int matrix[row][col];
+  
+  //Создание матрицы
+  for (int i=0; i<row; i++)
+  {
+    for (int j=0; j<col; j++)
+    {
+      matrix[i][j]=rand()%5;
+    }
+  }
+  
+    //Вывод матрицы в консоль   
+    for (int i=0; i<row; i++)
+      {
+        for (int j=0; j<col; j++)
+        {
+          cout<<matrix[i][j]<<'\t';
+        }
+        cout<<endl;
+      }
+      cout<<endl;
+    
+     
+    for (int i=0; i<row; i++)
+      {
+        for (int j=0; j<row-1; j++)
+        {
+         sum = sum + matrix[j][i]*matrix[j+1][i]; 
+		 sum = sum + matrix[i][j]*matrix[i][j+1];
+        }
+        sum = sum + matrix[i][row-1]*matrix[i][0]; 
+		sum = sum + matrix[row-1][i]*matrix[0][i]; 
+      }
+      
+		 
+  //Выводим сумму в консоль
+  cout<<sum;
+
+  return sum;
+}
