fe9fe7c0a8ee2a765ffe711c834cc9e77276e6f0
Кирилл <myakiishevkirill@gmail.com>
2022-05-06 12:41:06 +1000

    fixed syntax

--- sum_matrix_2var.cpp
+++ sum_matrix_2var.cpp
@@ -25,33 +25,32 @@ int main()
         }
     }
   
-  
-        //Вывод матрицы в консоль   
-        for (int i=0; i<row; i++)
+    //Вывод матрицы в консоль   
+    for (int i=0; i<row; i++)
+    {
+        for (int j=0; j<col; j++)
         {
-            for (int j=0; j<col; j++)
+        	if (matrix[i][j]==0)
             {
-        	    if (matrix[i][j]==0)
-                {
-                    matrix[i][j]= -1;
-                }
-                cout<<matrix[i][j]<<'\t';    
+                matrix[i][j]= -1;
             }
-            cout<<endl;
+            cout<<matrix[i][j]<<'\t';    
         }
         cout<<endl;
+    }
+    cout<<endl;
     
      
-        for (int i=0; i<row; i++)
-        {
-            for (int j=0; j<row-1; j++)
-            {  
+    for (int i=0; i<row; i++)
+    {
+        for (int j=0; j<row-1; j++)
+        {  
             sum = sum + matrix[j][i]*matrix[j+1][i]; 
 		    sum = sum + matrix[i][j]*matrix[i][j+1];
-            }
+        }
         sum = sum + matrix[i][row-1]*matrix[i][0]; 
 		sum = sum + matrix[row-1][i]*matrix[0][i]; 
-        }
+    }
       
 		 
     //Выводим сумму в консоль
